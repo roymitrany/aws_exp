2ed2ad183e0fc89669c4dd719b25fd949481a67c
Roy Mitrany <roym@technion.ac.il>
2020-02-24 16:18:58 +0000

    added basic Pub, basicBud and blootoothctl scripts

---
+++ basicPub.py
@@ -0,0 +1,96 @@
+'''
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from random import randint
+import sys
+import time
+import getopt
+import json
+
+# Read in command-line parameters
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+	if len(opts) == 0:
+		raise getopt.GetoptError("No input parameters!")
+	for opt, arg in opts:
+		if opt in ("-e", "--endpoint"):
+			host = arg
+		if opt in ("-r", "--rootCA"):
+			rootCAPath = arg
+		if opt in ("-c", "--cert"):
+			certificatePath = arg
+		if opt in ("-k", "--key"):
+			privateKeyPath = arg
+except getopt.GetoptError:
+	print(usageInfo)
+	exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+	print("Missing '-e' or '--endpoint'")
+	missingConfiguration = True
+if not rootCAPath:
+	print("Missing '-r' or '--rootCA'")
+	missingConfiguration = True
+if not certificatePath:
+	print("Missing '-c' or '--cert'")
+	missingConfiguration = True
+if not privateKeyPath:
+	print("Missing '-k' or '--key'")
+	missingConfiguration = True
+if missingConfiguration:
+	exit(2)
+
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+
+myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+myAWSIoTMQTTClient.connect()
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+while True:
+	data = {}
+	data['id'] = str(1)
+	data['timestamp'] = str(time.time())
+	data['info'] = {}
+	data['info']['temp'] = str(loopCount)
+	data['info']['humidity'] = randint(0,99)
+	json_data = json.dumps(data)
+	print(json_data)
+	
+	myAWSIoTMQTTClient.publish("nssl/test/Pub", json_data, 1)
+	loopCount += 1
+	time.sleep(3)
---
+++ basicSub.py
@@ -0,0 +1,122 @@
+'''
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import sys
+import logging
+import time
+import getopt
+import json
+from bluetoothctl import Bluetoothctl 
+
+power_mod = 1
+bl = Bluetoothctl()
+
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+	global power_mod
+	print("Received a new message: ")
+	print(message.payload)
+	print("from topic: ")
+	print(message.topic)
+	print("--------------\n\n")
+        bl.start_scan()
+        print("Scanning for 5 seconds...")
+        for i in range(0, 5):
+                print(i)
+                time.sleep(1)
+        list_of_bt = bl.get_available_devices()
+        for dev in list_of_bt:
+                print ("dev addr is: " + dev['mac_address'])
+	
+
+# Read in command-line parameters
+useWebsocket = False
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+	if len(opts) == 0:
+		raise getopt.GetoptError("No input parameters!")
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print(helpInfo)
+			exit(0)
+		if opt in ("-e", "--endpoint"):
+			host = arg
+		if opt in ("-r", "--rootCA"):
+			rootCAPath = arg
+		if opt in ("-c", "--cert"):
+			certificatePath = arg
+		if opt in ("-k", "--key"):
+			privateKeyPath = arg
+except getopt.GetoptError:
+	print(usageInfo)
+	exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+	print("Missing '-e' or '--endpoint'")
+	missingConfiguration = True
+if not rootCAPath:
+	print("Missing '-r' or '--rootCA'")
+	missingConfiguration = True
+if not useWebsocket:
+	if not certificatePath:
+		print("Missing '-c' or '--cert'")
+		missingConfiguration = True
+	if not privateKeyPath:
+		print("Missing '-k' or '--key'")
+		missingConfiguration = True
+if missingConfiguration:
+	exit(2)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+
+myAWSIoTMQTTClient = AWSIoTMQTTClient("basicSub")
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe("nssl/device/dev1", 1, customCallback)
+
+loopCount = 0
+# Publish to the same topic in a loop
+while loopCount < 500:
+	print loopCount
+	print("power mod is {0}".format(power_mod))
+	sys.stdout.flush()
+	loopCount += 1
+	time.sleep(10)
+print "This is it!!"
+exit(0)
+
+
+
